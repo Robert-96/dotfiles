80aa157765f5f5809ab34b4804cc9997f2d577fc
Robert-96 <dezmereanrobert@gmail.com>
2022-06-18 00:13:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo ">>> Copy vim config..."
+echo ""
+cp vim/.vimrc $HOME/.vimrc
+
 echo ">>> Install py-prompts..."
 echo ""
 curl -L https://raw.githubusercontent.com/Robert-96/py-prompts/main/install.sh | sh
