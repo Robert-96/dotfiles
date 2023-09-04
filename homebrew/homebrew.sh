268e949fc2f3d88ee0d40ded1c6104fb8048b85e
Dezmerean Robert <dezmereanrobert@gmail.com>
2023-09-04 11:46:28 +0300

    Create homebrew.sh

---
+++ homebrew/homebrew.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install command-line tools using Homebrew.
+brew install bat
+brew install tree
