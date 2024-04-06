bd47e171d011d34530428b5c4afda140194c504e
Dezmerean Robert <dezmereanrobert@gmail.com>
2024-04-06 22:50:20 +0300

    Create python/install.sh

---
+++ python/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pip install pipx 
+
+pipx install pylint
+pipx install black
+pipx install poetry
+pipx install pipenv
+pipx install bandit
+pipx install mypy
+pipx install flake8
+pipx install altwalker 
